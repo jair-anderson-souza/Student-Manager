60ce5f4fd3bfee57e73057fa8c33d87c8d0f2a0f
Anderson Souza <jair_anderson_bs@hotmail.com>
2017-05-26 10:56:25 -0300

    services without modular strutucture project

--- Web/client/App.js
+++ Web/client/App.js
@@ -4,9 +4,35 @@ app.config(function($routeProvider) {
 	$routeProvider.when("/new", {
 		templateUrl: "src/webapp/views/new.html",
 		controller: "NewCtrl"
+	}).when("/list", {
+		templateUrl: "src/webapp/views/list.html",
+		controller: "ListCtrl"
 	});
 });
 
 app.controller("NewCtrl", function($scope){
+	$scope.name = "New";
+});
+
+app.controller("ListCtrl", function($scope, ListService){
+	$scope.students = ListService.getStudents();
+});
+
+app.service("ListService", function(){
+	var students = [{
+		name : "Anderson",
+		age: "21"
+	}];
+
+	this.getStudents = function(){
+		return students;
+	};
+})
+/*
+app.controller("NewCtrl", function($scope){
 	$scope.name = "Anderson";
 });
+
+app.controller("NewCtrl", function($scope){
+	$scope.name = "Anderson";
+});*/
