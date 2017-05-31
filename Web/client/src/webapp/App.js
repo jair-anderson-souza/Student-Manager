32cd8d8c3cfd3f5b12590442e5292e1c7c8271c9
Anderson Souza <jair_anderson_bs@hotmail.com>
2017-05-31 19:43:51 -0300

    validation fields in html page

--- Web/client/src/webapp/App.js
+++ Web/client/src/webapp/App.js
@@ -1,1 +1,36 @@
-angular.module("App")
+var app = angular.module("App", ["ngRoute"]);
+
+app.config(function($routeProvider) {
+	$routeProvider.when("/new", {
+		templateUrl: "src/webapp/views/new.html",
+		controller: "NewCtrl"
+	}).when("/list", {
+		templateUrl: "src/webapp/views/list.html",
+		controller: "ListCtrl"
+	});
+});
+
+app.controller("NewCtrl", function($scope, ListService){
+	$scope.student = [];
+	var student = $scope.student;
+	$scope.add = ListService.addStudent(student);
+});
+
+app.controller("ListCtrl", function($scope, ListService){
+	$scope.students = ListService.getStudents();
+});
+
+app.service("ListService", function(){
+	var students = [{
+		name : "Anderson",
+		age: "21"
+	}];
+
+	this.addStudent = function(student){
+		students.push(student);
+	};
+
+	this.getStudents = function(){
+		return students;
+	};
+});
